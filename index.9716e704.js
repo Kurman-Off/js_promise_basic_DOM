30517c47547ecb1dd020d170247754d12c3cd194
Your Name <ruslankurman13@gmail.com>
2024-08-05 18:35:35 +0300

    make build

---
+++ index.9716e704.js
@@ -0,0 +1,2 @@
+var e=new Promise(function(e,t){document.querySelector(".logo").addEventListener("click",function(){e()})}),t=new Promise(function(e,t){setTimeout(function(){t(Error("Promise was rejected!"))},3e3)}),n=function(){var e=document.createElement("div");e.className="message",e.textContent="Promise was resolved!",document.body.appendChild(e)},o=function(){var e=document.createElement("div");e.className="error-message",e.textContent="Promise was rejected!",document.body.appendChild(e)};e.then(n).catch(o),t.then(n).catch(o);
+//# sourceMappingURL=index.9716e704.js.map
